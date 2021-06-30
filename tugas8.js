d1513614874ed1f9ae88d1415c94443cce4092b0
Helmianto <helmianto15@gmail.com>
2021-06-30 16:23:11 +0700

    initital commit

---
+++ tugas8.js
@@ -0,0 +1,11 @@
+const mobil = {
+    merk: 'Toyota',
+    jenis: 'Avanza',
+    warna: 'Hitam'
+};
+
+console.log('Objek sebelum dirubah nilainya: ', mobil);
+
+mobil.warna = 'Silver';
+
+console.log('Objek setelah dirubah nilainya: ', mobil);
